9ec405144aea5b0f7b59ed759bf747f34f272873
Dave <dapitlast@gmail.com>
2018-01-08 16:13:30 +0700

    Fix duration format

--- api/src/main/java/com/ninelives/insurance/api/util/DateTimeFormatUtil.java
+++ api/src/main/java/com/ninelives/insurance/api/util/DateTimeFormatUtil.java
@@ -6,7 +6,7 @@ import org.apache.commons.lang3.time.DurationFormatUtils;
 
 public class DateTimeFormatUtil {
 	
-	public static String DURATION_FORMAT = "H 'jam' m 'minutes'";
+	public static String DURATION_FORMAT = "H 'jam' m 'menit'";
 	
 	public static String timeBetween(LocalDateTime start, LocalDateTime end){
 		if(start == null || end == null){
