ec4d9e329d0c7346c1da92848a2d04173846dbc8
Dave <dapitlast@gmail.com>
2018-01-22 11:25:04 +0700

    Allow http for development

---
+++ api/src/main/java/com/ninelives/insurance/api/ServletContainerConfig.java
@@ -0,0 +1,35 @@
+package com.ninelives.insurance.api;
+
+import org.apache.catalina.connector.Connector;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("dev")
+public class ServletContainerConfig {
+	@Value("${server.http.port}")
+	private int httpPort;
+
+	@Bean
+	public EmbeddedServletContainerCustomizer containerCustomizer() {
+	    return new EmbeddedServletContainerCustomizer() {
+
+			@Override
+			public void customize(ConfigurableEmbeddedServletContainer container) {
+				if (container instanceof TomcatEmbeddedServletContainerFactory) {
+	                TomcatEmbeddedServletContainerFactory containerFactory =
+	                        (TomcatEmbeddedServletContainerFactory) container;
+
+	                Connector connector = new Connector(TomcatEmbeddedServletContainerFactory.DEFAULT_PROTOCOL);
+	                connector.setPort(httpPort);
+	                containerFactory.addAdditionalTomcatConnectors(connector);
+	            }
+			}
+	    };
+	}
+}
