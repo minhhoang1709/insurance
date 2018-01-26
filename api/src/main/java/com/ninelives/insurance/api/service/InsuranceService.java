ff6673996c80df75fcd26f7181cc13dd224b35e4
Dave <dapitlast@gmail.com>
2018-01-26 17:52:23 +0700

    Fix order number field name

--- api/src/main/java/com/ninelives/insurance/api/service/InsuranceService.java
+++ api/src/main/java/com/ninelives/insurance/api/service/InsuranceService.java
@@ -33,7 +33,7 @@ public class InsuranceService {
 				ResponseDto<OrderResponseDto> result = insuranceProvider.orderPolicy(order);
 				if(insuranceProvider.isSuccess(result)){
 					order.setPolicyNumber(result.getResponse().getResponseParam().getPolicyNumber());
-					order.setProviderOrderNumber(result.getResponse().getResponseParam().getOderNumber());
+					order.setProviderOrderNumber(result.getResponse().getResponseParam().getOrderNumber());
 				}else{
 					throw new ApiInternalServerErrorException(ErrorCode.ERR4201_ORDER_PROVIDER_FAIL, "Permintaan tidak dapat diproses, terjadi error pada sistem");
 				}
