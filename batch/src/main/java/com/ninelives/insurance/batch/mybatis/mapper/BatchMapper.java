76e39d03798660770ed0b3fc2481b3885d8fa0d4
Dave <dapitlast@gmail.com>
2018-03-26 15:48:21 +0700

    Fix batch bug on retrieving expired target

--- batch/src/main/java/com/ninelives/insurance/batch/mybatis/mapper/BatchMapper.java
+++ batch/src/main/java/com/ninelives/insurance/batch/mybatis/mapper/BatchMapper.java
@@ -24,7 +24,7 @@ public interface BatchMapper {
 		"SELECT b.user_id, b.email, b.fcm_token, a.order_id, c.coverage_category_id, c.name coverage_category_name ",
 		"FROM public.policy_order a, public.users b, public.coverage_category c ",
 		"WHERE b.user_id=a.user_id and c.coverage_category_id=a.coverage_category_id ",
-		"  and a.policy_end_date=#{targetDate,jdbcType=DATE} ",
+		"  and a.policy_end_date=(#{targetDate,jdbcType=DATE}::date - '1 day'::interval) ",
 		"  and a.status in ('APPROVED','ACTIVE')"
 	})
 	public PushNotificationData selectExpireOrder(@Param("targetDate") LocalDate targetDate);
