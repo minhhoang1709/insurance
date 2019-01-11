8d57f3f6b776591c39f2155f02a5a3f2279a8442
owner <owner@Hey-Kompiute>
2019-01-11 13:39:31 +0700

    Add coverage category type for new coverage category

--- common-lib/src/main/java/com/ninelives/insurance/ref/CoverageCategoryId.java
+++ common-lib/src/main/java/com/ninelives/insurance/ref/CoverageCategoryId.java
@@ -4,4 +4,5 @@ public class CoverageCategoryId {
 	public static final String PERSONAL_ACCIDENT = "101";
 	public static final String TRAVEL_INTERNATIONAL = "102";
 	public static final String TRAVEL_DOMESTIC = "103";
+	public static final String SELFIE = "104";
 }
