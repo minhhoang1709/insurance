910bd2e383a2c49cda6463dc9065923e9fbf7a6a
Dave <dapitlast@gitlab.com>
2019-01-29 19:20:58 +0700

    Fix push notif for travel insurance
    *  Apply D-0 for 1 to 3 days policy period
    *  Apply D-2 for the rest of travel policy period

--- batch/src/main/java/com/ninelives/insurance/batch/mybatis/mapper/BatchMapper.java
+++ batch/src/main/java/com/ninelives/insurance/batch/mybatis/mapper/BatchMapper.java
@@ -31,17 +31,39 @@ public interface BatchMapper {
 	})
 	public PushNotificationData selectExpireOrderForFcmNotification(@Param("targetDate") LocalDate targetDate);
 	
-	@Select ({
-		"SELECT b.user_id, b.email, b.fcm_token, a.order_id, c.coverage_category_id, c.name coverage_category_name ",
-		"FROM public.policy_order a, public.users b, public.coverage_category c ",
-		"WHERE b.user_id=a.user_id and c.coverage_category_id=a.coverage_category_id ",
-		  "and (",
-		    "(a.policy_end_date=(#{targetDate,jdbcType=DATE}::date) and a.period_id='101') ",
-		      "or ",
-		    "(a.policy_end_date=(#{targetDate,jdbcType=DATE}::date + '2 day'::interval) and a.period_id!='101') ",
-		  ") ",
-		  "and a.status in ('APPROVED','ACTIVE') ",
-		  "and b.fcm_token is not null"
+//	@Select ({
+//		"SELECT b.user_id, b.email, b.fcm_token, a.order_id, c.coverage_category_id, c.name coverage_category_name ",
+//		"FROM public.policy_order a, public.users b, public.coverage_category c ",
+//		"WHERE b.user_id=a.user_id and c.coverage_category_id=a.coverage_category_id ",
+//		  "and (",
+//		    "(a.policy_end_date=(#{targetDate,jdbcType=DATE}::date) and a.period_id='101') ",
+//		      "or ",
+//		    "(a.policy_end_date=(#{targetDate,jdbcType=DATE}::date + '2 day'::interval) and a.period_id!='101') ",
+//		  ") ",
+//		  "and a.status in ('APPROVED','ACTIVE') ",
+//		  "and b.fcm_token is not null"
+//	})
+//	
+	@Select({
+		"SELECT a.user_id, a.email, a.fcm_token, a.order_id, a.coverage_category_id, a.coverage_category_name ",
+		"FROM ( ",
+		  "SELECT b.user_id, b.email, b.fcm_token, a.order_id, c.coverage_category_id, c.name coverage_category_name,  ",
+		     " a.period_id, a.policy_end_date, (a.policy_end_date-a.policy_start_date)+1 as policy_days  ",
+		  "FROM public.policy_order a, public.users b, public.coverage_category c  ",
+		  "WHERE b.user_id=a.user_id and c.coverage_category_id=a.coverage_category_id  ",
+		      "and a.policy_start_date<=(#{targetDate,jdbcType=DATE}::date) ",
+		      "and a.policy_end_date>=(#{targetDate,jdbcType=DATE}::date) ",
+		      "and a.status in ('APPROVED','ACTIVE') ",
+		") a ",
+		"WHERE ( ",
+		  "( a.policy_end_date=(#{targetDate,jdbcType=DATE}::date) and a.period_id='101' )  ",
+		  "or ",
+		  "( a.policy_end_date=(#{targetDate,jdbcType=DATE}::date) and a.period_id ='201' and a.policy_days between 1 and 3 ) ",
+		  "or ",
+		  "( a.policy_end_date=(#{targetDate,jdbcType=DATE}::date + '2 day'::interval) and a.period_id ='201' and a.policy_days > 3 ) ",
+		  "or ",
+		  "( a.policy_end_date=(#{targetDate,jdbcType=DATE}::date + '2 day'::interval) and a.period_id not in ('101','201') ) ",
+		")  ",
 	})
 	public PushNotificationData selectToBeExpireOrderForFcmNotification(@Param("targetDate") LocalDate targetDate);
 }
