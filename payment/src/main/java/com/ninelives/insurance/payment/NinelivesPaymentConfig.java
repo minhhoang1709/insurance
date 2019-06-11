ececb912e7c88099e980a903d73b09727c69b484
Dave <dapitlast@gitlab.com>
2019-06-11 16:29:51 +0700

    Add debug request filter to payment module

--- payment/src/main/java/com/ninelives/insurance/payment/NinelivesPaymentConfig.java
+++ payment/src/main/java/com/ninelives/insurance/payment/NinelivesPaymentConfig.java
@@ -3,7 +3,10 @@ package com.ninelives.insurance.payment;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
@@ -11,4 +14,18 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 public class NinelivesPaymentConfig extends WebMvcConfigurerAdapter{
 	private static final Logger logger = LoggerFactory.getLogger(NinelivesPaymentConfig.class);
 	
+	/*
+	 * For debugging on development
+	 */
+	@Bean
+	@Profile("dev")
+	public CommonsRequestLoggingFilter requestLoggingFilter() {
+	    CommonsRequestLoggingFilter loggingFilter = new CommonsRequestLoggingFilter();
+	    loggingFilter.setIncludeHeaders(true);
+	    loggingFilter.setIncludeClientInfo(true);
+	    loggingFilter.setIncludeQueryString(true);
+	    loggingFilter.setIncludePayload(true);
+	    loggingFilter.setMaxPayloadLength(10000);
+	    return loggingFilter;
+	}
 }
